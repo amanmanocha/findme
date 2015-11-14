6721ae95c6fed8b55a238147054ac10696cbbdab
Aman <amanmanocha@yahoo.com>
2015-11-14 15:08:48 +0800

    Use findme-model instead of domain

---
+++ findme-model/src/main/java/sample/persistence/AddUserCommand.java
@@ -0,0 +1,5 @@
+package sample.persistence;
+
+public class AddUserCommand {
+
+}
---
+++ findme-model/src/main/java/sample/persistence/Command.java
@@ -0,0 +1,16 @@
+package sample.persistence;
+
+import java.io.Serializable;
+
+public class Command implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private final String data;
+
+    public Command(String data) {
+        this.data = data;
+    }
+
+    public String getData() {
+        return data;
+    }
+}
