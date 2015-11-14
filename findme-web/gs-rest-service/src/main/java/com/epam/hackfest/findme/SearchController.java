65ea34a68d2675403f9c7f38701bc25c16462343
Aman <amanmanocha@yahoo.com>
2015-11-14 22:26:39 +0800

    Search returns immidiately

--- findme-web/gs-rest-service/src/main/java/com/epam/hackfest/findme/SearchController.java
+++ findme-web/gs-rest-service/src/main/java/com/epam/hackfest/findme/SearchController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.common.base.Optional;
+
 import sample.persistence.RegisterUserCommand;
 import sample.persistence.SearchUserCommand;
 import sample.persistence.SetOldNumberCommand;
@@ -19,41 +21,49 @@ import findme.model.User;
 public class SearchController {
 
 	@RequestMapping("/search")
-	public User search(@RequestParam(value = "name", required = true, defaultValue = "") String name,
-			@RequestParam(value = "phone", required = true, defaultValue = "") String phone) throws Exception {
+	public User search(
+			@RequestParam(value = "name", required = true, defaultValue = "") String name,
+			@RequestParam(value = "phone", required = true, defaultValue = "") String phone)
+			throws Exception {
 
 		String[] parts = name.split(" ");
 		String firstName = parts[0];
 		String secondName = "";
-		if(parts.length > 1)
+		if (parts.length > 1)
 			secondName = parts[1];
-		
-		
+
 		Timeout timeout = new Timeout(Duration.create(5, "minutes"));
 		Future<Object> future = Patterns.ask(Application.getActor(),
-				new SearchUserCommand(firstName, secondName, new PhoneNumber(phone)),
-				10 * 1000);
+				new SearchUserCommand(firstName, secondName, new PhoneNumber(
+						phone)), 10 * 1000);
 
-		User result = (User) Await.result(future, timeout.duration());
-		System.out.println(result);
- 
-		return result;
+		@SuppressWarnings("unchecked")
+		Optional<User> result = (Optional<User>) Await.result(future,
+				timeout.duration());
+		if (result.isPresent()) {
+			return result.get();
+		}
+		return null;
+
 	}
 
 	@RequestMapping("/register")
-	public ResultState register(@RequestParam(value = "name", required = true, defaultValue = "") String name,
+	public ResultState register(
+			@RequestParam(value = "name", required = true, defaultValue = "") String name,
 			@RequestParam(value = "phone", required = true, defaultValue = "") String phone,
-			@RequestParam(value = "password", required = true, defaultValue = "") String password) throws Exception {
+			@RequestParam(value = "password", required = true, defaultValue = "") String password)
+			throws Exception {
 		String[] parts = name.split(" ");
 		String firstName = parts[0];
 		String secondName = "";
-		if(parts.length > 1)
+		if (parts.length > 1)
 			secondName = parts[1];
-		
-		RegisterUserCommand registerUserCommand = new RegisterUserCommand(firstName, secondName, new PhoneNumber(phone));
+
+		RegisterUserCommand registerUserCommand = new RegisterUserCommand(
+				firstName, secondName, new PhoneNumber(phone));
 
 		Application.getActor().tell(registerUserCommand, null);
- 
+
 		return new ResultState(0, "OK");
 	}
 
@@ -61,10 +71,13 @@ public class SearchController {
 	public ResultState addOldNumber(
 			@RequestParam(value = "curr", required = true, defaultValue = "") String currentPhone,
 			@RequestParam(value = "old", required = true, defaultValue = "") String oldPhone,
-			@RequestParam(value = "isPrivate", required = true, defaultValue = "") boolean isCurrentPhonePrivate) throws Exception {
-		SetOldNumberCommand setOldNumberCommand = new SetOldNumberCommand(!isCurrentPhonePrivate, new PhoneNumber(currentPhone), new PhoneNumber(oldPhone));
+			@RequestParam(value = "isPrivate", required = true, defaultValue = "") boolean isCurrentPhonePrivate)
+			throws Exception {
+		SetOldNumberCommand setOldNumberCommand = new SetOldNumberCommand(
+				!isCurrentPhonePrivate, new PhoneNumber(currentPhone),
+				new PhoneNumber(oldPhone));
 		Application.getActor().tell(setOldNumberCommand, null);
- 
+
 		return new ResultState(0, "OK");
 	}
 }
